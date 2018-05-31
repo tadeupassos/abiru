18e097b937ac2868a1c667a9f1370a6cdbda5af5
Tadeu Passos <tadeu.passos@gmail.com>
2018-05-30 22:02:11 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,47 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpModule } from '@angular/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AbbasPage } from '../pages/abbas/abbas';
+import { VegetaisPage } from '../pages/vegetais/vegetais';
+import { FrutasPage } from '../pages/frutas/frutas';
+import { CarrinhoPage } from '../pages/carrinho/carrinho';
+
+import { ServicosProvider } from '../providers/servicos/servicos';
+
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    VegetaisPage,
+    FrutasPage,
+    AbbasPage,
+    CarrinhoPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    VegetaisPage,
+    FrutasPage,
+    AbbasPage,
+    CarrinhoPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    ServicosProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ModalController } from 'ionic-angular';
+import { ServicosProvider } from '../../providers/servicos/servicos';
+import { CarrinhoPage } from '../carrinho/carrinho';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +9,29 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  searchTerm: string = '';
+  items: any; 
+
+  constructor(public navCtrl: NavController, public serv: ServicosProvider, public modalCtrl: ModalController) {
+
+  }
+
+  ionViewDidLoad() {
+ 
+    this.setFilteredItems();
+
+  }
+
+  setFilteredItems() {
+ 
+    this.items = this.serv.filterItems(this.searchTerm);
+
+  } 
+
+  mostrarCarrinho(){
+
+    let modal = this.modalCtrl.create(CarrinhoPage);
+    modal.present();
 
   }
 
